92846b8463a0c2c2a49c7dc61402162ec0aef0c0
khaled365-code <khaleddortmound123@gmail.com>
2022-08-15 20:59:08 +0200

    Initial commit

---
+++ app/src/main/java/com/khaledmohamed/myclinicjop/addressActivity2.kt
@@ -0,0 +1,11 @@
+package com.khaledmohamed.myclinicjop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class addressActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_address2)
+    }
+}
